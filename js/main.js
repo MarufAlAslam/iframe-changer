df86e61a001379518230579bf6600c39455a0e61
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-01-14 17:27:12 +0600

    .

--- js/main.js
+++ js/main.js
@@ -13,13 +13,56 @@ $('.optionBtn').click(function(){
     $(this).parent().parent().children('.option__control').show()
 })
 
+if($(window).width()>=768){
+    $('.menuBtn, label.texture').click(function(){
+        $('.col-md-3').toggle()
+        $('.col-md-9').toggleClass('wider')
+    })
+    
+    $('label.texture').click(function(){
+        $('.col-md-3').hide()
+        $('.col-md-9').addClass('wider')
+    
+        basePrice = 100
+        val = Number($('#amount').html())
+        inc = Number($(this).children('.text-white').children('.inc').html())
+        $('#amount').html(basePrice+inc)
+    })
+}
 
-$('.menuBtn, label.texture').click(function(){
-    $('.col-md-3').toggle()
-    $('.col-md-9').toggleClass('wider')
+else{
+    $('.menuBtn, label.texture').click(function(){
+        $('.col-md-3').toggle()
+        $('.col-md-9').toggleClass('wider')
+    })
+    $('label.texture').click(function(){
+        basePrice = 100
+        val = Number($('#amount').html())
+        inc = Number($(this).children('.text-white').children('.inc').html())
+        $('#amount').html(basePrice+inc)
+    })
+}
+
+
+
+
+$('.btn-add').click(function(e){
+    e.preventDefault()
+    basePrice = 100
+    val = Number($('#amount').html())
+    inc = Number($(this).parent().children('.text-white').children('.inc').html())
+    $('#amount').html(basePrice+inc)
+    
+
 })
 
-$('label.texture').click(function(){
-    $('.col-md-3').hide()
-    $('.col-md-9').addClass('wider')
+
+
+
+
+$('.optionBtn').click(function(){
+    $('.option__control').removeClass('flx')
+    $(this).parent().parent().children('.option__control').addClass('flx')
 })
+
+
