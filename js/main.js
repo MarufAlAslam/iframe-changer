2a981581ca376431aa23a99bfe715cc8da782530
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-01-14 19:23:30 +0600

    .

--- js/main.js
+++ js/main.js
@@ -13,20 +13,38 @@ $('.optionBtn').click(function(){
     $(this).parent().parent().children('.option__control').show()
 })
 
+
+var inc = 0
+var inc1 = 0
+var price = 0
+
 if($(window).width()>=768){
     $('.menuBtn, label.texture').click(function(){
         $('.col-md-3').toggle()
         $('.col-md-9').toggleClass('wider')
     })
     
-    $('label.texture').click(function(){
+    $('label.texture.txtr').click(function(){
         $('.col-md-3').hide()
         $('.col-md-9').addClass('wider')
     
         basePrice = 100
         val = Number($('#amount').html())
         inc = Number($(this).children('.text-white').children('.inc').html())
-        $('#amount').html(basePrice+inc)
+        // inc1 = Number($(this).children('.text-white').children('.inc1').html())
+
+        price = basePrice+inc+inc1
+        // console.log(price)
+        $('#amount').html(price)
+    })
+    $('label.texture.txtr1').click(function(){
+        basePrice = 100
+        val = Number($('#amount').html())
+        inc1 = Number($(this).children('.text-white').children('.inc1').html())
+        console.log(inc1)
+        // inc1 = Number($(this).children('.text-white').children('.inc1').html())
+        price = basePrice+inc+inc1
+        $('#amount').html(price)
     })
 }
 
@@ -35,26 +53,37 @@ else{
         $('.col-md-3').toggle()
         $('.col-md-9').toggleClass('wider')
     })
-    $('label.texture').click(function(){
+    $('label.texture.txtr').click(function(){
         basePrice = 100
         val = Number($('#amount').html())
         inc = Number($(this).children('.text-white').children('.inc').html())
-        $('#amount').html(basePrice+inc)
+
+        // inc1 = Number($(this).children('.text-white').children('.inc1').html())
+        price = basePrice+inc+inc1
+        // console.log(price)
+        $('#amount').html(price)
+    })
+    $('label.texture.txtr1').click(function(){
+        basePrice = 100
+        val = Number($('#amount').html())
+        inc1 = Number($(this).children('.text-white').children('.inc1').html())
+        console.log(inc1)
+        // inc1 = Number($(this).children('.text-white').children('.inc1').html())
+        price = basePrice+inc+inc1
+        $('#amount').html(price)
     })
 }
 
 
 
 
-$('.btn-add').click(function(e){
-    e.preventDefault()
-    basePrice = 100
-    val = Number($('#amount').html())
-    inc = Number($(this).parent().children('.text-white').children('.inc').html())
-    $('#amount').html(basePrice+inc)
-    
-
-})
+// $('.btn-add').click(function(e){
+//     e.preventDefault()
+//     basePrice = 100
+//     val = Number($('#amount').html())
+//     inc = Number($(this).parent().children('.text-white').children('.inc').html())
+//     $('#amount').html(basePrice+inc)
+// })
 
 
 
